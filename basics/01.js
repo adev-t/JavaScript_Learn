eaaeeaebb23c9652ad0fd28e259c0ee567650943
advait <advaitsingh.55@gmail.com>
2024-09-28 10:13:27 +0000

    ese hi -_- idk

--- basics/01.js
+++ basics/01.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+// variable declation...
+const user_Id = 32;
+let email_Id = "helloworld@gmail.com";
+let user_password = "12345678";
+var user_address = "i dont know man...near i dont know man";
+var user_phone = 859839589829
+
+// const cant be changed after being assigned.
+
+console.log(user_Id);
+
+console.table(user_password, user_address, user_phone)
+
+
