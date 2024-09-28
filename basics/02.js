f13bd16b880fba11eeca2b35fc3d5e131a3d766e
advait <advaitsingh.55@gmail.com>
2024-09-28 12:54:10 +0000

    um hmm

--- basics/02.js
+++ basics/02.js
@@ -1,19 +1,29 @@
 // title_________________
 
-let kills = "33";
+let score = "33";
 // const {score} = req.body  [requested frontend to give the score. but it may/may not be of desired datatype.]
 
-console.log(typeof score);
-console.log(typeof(score));
 
 let value_in_number = Number(score);  // guarantee that the score is a number type.
 console.log(typeof value_in_number);
 
 // ============================================================================================
 
-let score = "33abc"
-value_in_number_score = Number(score)
+let kills = "33abc"
+value_in_number_kills = Number(kills)
 
+console.log(typeof kills);
+console.log(typeof(kills));
+
+// "33" --> 33
+// "33erws" --> NaN (TYPE = NUMBER)
+// true --> 1
+// false --> 0
+// Null --> 0
+
+// what if number to boolean..?
+// 1 - true, 0 - false
+// "" - false, "nkdw" - true
 
 
 
